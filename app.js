581ed9767585d7205ec0d1aaf80daacf64b2715a
keunHyeok jeon <kh.jeon@how2marry.com>
2016-05-13 20:00:21 +0900

    'ha'

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ var ejs = require('ejs-locals');
 
 var app = express();
 
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 app.engine('ejs', ejs);
-//haha
-//gdaga
+
+
 app.get('/',function(req,res) {
 	res.render('index');
 });
